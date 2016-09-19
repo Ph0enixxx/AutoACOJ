628625d72a14bbc9f2b09686b67e40da58389c59
shaoyingshuai <754406613@qq.com>
2016-09-19 18:33:55 +0800

    init

---
+++ Solution.py
@@ -0,0 +1,55 @@
+import requests
+import re
+import sys
+import io
+from bs4 import BeautifulSoup
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+
+
+#登陆 保持session
+#获取题目列表
+#查找题目
+#提交
+#
+
+#提交试题类
+#login submit check
+#抽象出基类
+#try！ catch
+
+class BaseSolution(object):
+	def __init__(self,sid,username,password,autosubmit=False):
+		self.session = requests.Session()
+		self.sid = str(sid)
+		login = self._login(username,password)
+		print(login)
+		pass
+	def _login(self,username,password):
+		res = self.session.post(self._loginUrl,data = self._loginData)
+		if username in res.text:
+			return "ok~~~"
+		return None
+	def submit(self,sid,code):
+		pass
+	def check(self):
+		pass
+
+class Solution(BaseSolution):
+	def __init__(self,sid,username,password,autosubmit=False):
+		self._loginUrl = "http://acm.hdu.edu.cn/userloginex.php?action=login"
+		self._loginData = {"username":username,"userpass":password}
+		super().__init__(sid,username,password,autosubmit)
+		#http://acm.hdu.edu.cn/userloginex.php?action=login  username   userpass
+		pass
+	def submit(self,code):
+		payload = {}
+		payload['problemid'] = str(self.sid)
+		payload['language']  = str(1)
+		payload['usercode']  = str(code)
+		res = self.session.post("http://acm.hdu.edu.cn/submit.php?action=submit",data=payload)
+	# def check(self):
+	# 	res  = requests.get("http://acm.hdu.edu.cn/status.php?first=&pid=&user=" + self.sid)
+	# 	soup = BeautifulSoup(res.text,"html.parser")
+	# 	#print(res.text)
+	# 	return (soup.table.find_all('tr'))
+	pass
---
+++ autoAC.py
@@ -0,0 +1,54 @@
+from Solution import Solution
+import requests 
+import re
+
+#查找代码类
+class AutoAC(object):
+	def __init__(self,sid,extra=" hdu ",url="blog.csdn.net"):
+		self.keyword = str(sid) + extra  + url
+		self.url = url
+		print(self.keyword)
+	def _search(self):
+		#https://wap.baidu.com/s?word=1001杭电+ac++site%3Acsdn.net
+		res = requests.get("http://cn.bing.com/search?q=" + (self.keyword))
+		#print(res.text)
+		text = re.findall(r"href=\"(http://" + self.url + ".*?)\"",res.text)
+		print(text[0])
+		return text[0]
+	def getCode(self,url=""):
+		#功能 
+		#优化！
+		#url = "http://blog.csdn.net/jopus/article/details/20471117"
+		if url == "":
+			url = self._search()
+		user_agent = {'User-agent': 'Mozilla/5.0'}
+		t = requests.get(url,headers=user_agent)
+		#1print(t)
+		code = re.findall(r"#include[.\s\S]*return 0;",t.text)
+		code = self._del_tags("".join(code))
+		print(code)
+		return code
+	def _del_tags(self,text):
+		#功能 ok
+		#优化！
+		#去掉文字
+		text = re.sub(r"<.*?>","",text)
+		text = re.sub(r"&lt;","<",text)
+		text = re.sub(r"&gt;",">",text)
+		text = re.sub(r"&quot;","\"",text)
+		text = re.sub(r"&amp;","&",text)
+		return (text+"\n}")
+	pass
+
+def main():
+	#text = req.get("")
+	s = Solution(1003,"201458503124","asdf1234")
+	#多线程！
+	#print(s.check())
+	a = AutoAC(1003)
+	code = a.getCode()
+	# code = a._getCode("123")
+	print(code)
+	s.submit(code)
+if __name__ == '__main__':
+	main()
